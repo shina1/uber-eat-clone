385a0b06458f7a58293af5e8357df5b7134db2e1
shina1 <oswasiu@gmail.com>
2022-09-17 23:13:27 +0100

    completed category section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import CategoryComp from './components/CategoryComp';
+import FooterComp from './components/FooterComp';
 import HeadlineComponent from './components/HeadlineComponent';
 import HeroComponent from './components/HeroComponent/HeroComponent';
 import Navbar from './components/NavbarComp/Navbar';
+import SubscriptionComp from './components/SubscriptionComp';
 import TopRatedMenuComp from './components/TopRatedMenuComp';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <HeroComponent />
       <HeadlineComponent />
       <TopRatedMenuComp />
+      <CategoryComp />
+      <SubscriptionComp />
+      <FooterComp />
     </div>
   );
 }
